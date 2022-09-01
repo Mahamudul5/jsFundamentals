4cc22583b84331e71bc08dc0c226bb08ca3b4e22
mahamudul5 <mh.bilash22@gmail.com>
2022-09-02 00:07:28 +0600

    problem2 add two numbers

--- jsFundamatal.js
+++ jsFundamatal.js
@@ -1,8 +1,21 @@
 //Prblem-1: Write a function that takes a value as argument
-// Return the type of the value
+// Return the type of the value???
 
 // solution:
-function myFunction(a) {
-    return console.log(typeof a);
+// function myFunction(a) {
+//     return console.log(typeof a);
+// }
+// myFunction(2);
+// ---------------------------------------------------------------------------------
+
+//Problem - 2: Write a function that takes two numbers (a and b) as argument
+// Sum a and b
+// Return the result???
+
+
+// solution:
+function addNumber(a, b) {
+    const result = a + b;
+    return console.log(result);
 }
-myFunction(2);
+addNumber(5, 6);
